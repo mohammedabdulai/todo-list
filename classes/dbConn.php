ea5170922fcfe1125080a3a3419cce32095aff90
mohammed abdulai <ma678@njit.edu>
2017-11-23 21:21:06 -0500

    database connection class added

---
+++ classes/dbConn.php
@@ -0,0 +1,35 @@
+<?php
+
+define('DATABASE', 'ma678');
+define('USERNAME', 'ma678');
+define('PASSWORD', 'vbBE9fgQM');
+define('CONNECTION', 'sql1.njit.edu');
+
+class dbConn{
+    //variable to hold connection object.
+    protected static $db;
+    //private construct - class cannot be instatiated externally.
+    private function __construct() {
+        try {
+            // assign PDO object to db variable
+            self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch (PDOException $e) {
+            //Output error - would normally log this to error file rather than output to user.
+            echo "Connection Error: " . $e->getMessage();
+        }
+    }
+    // get connection function. Static method - accessible without instantiation
+    public static function getConnection() {
+        //Guarantees single instance, if no connection object exists then create one.
+        if (!self::$db) {
+            //new connection object.
+            new dbConn();
+        }
+        //return connection.
+        return self::$db;
+    }
+}
+
+?>
