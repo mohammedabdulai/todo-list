a1de6ad28fcce78b1d8d4cb74c1739c4ecdf9f53
mohammed abdulai <ma678@njit.edu>
2017-12-01 16:36:49 -0500

    Bootstrap addded

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -1,28 +1,30 @@
-<!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
 <?php
-//this is how you print something
-print utility\htmlTable::genarateTableFromMultiArray($data);
+use utility\htmlTags;
+class all_tasks extends utility\page
+{
+    protected $page;
+
+    public function buildPage($content = '')
+    {
+        return $this->page .= $content . '<br>';
+
+    }
+
+}
+
+$newPage = new all_tasks();
+
+$header = '<div class="container"><h1>All Tasks</h1></div>';
+
+$links = '<div class="container">
+<h3><a href="index.php?page=accounts&action=all">Show All Accounts</a></h3></div>';
+
+
+$newPage->buildPage($header);
+$newPage->buildPage($links);
+$table= utility\htmlTable::genarateTableFromMultiArray($data);
+$newPage->buildPage($table);
+$newPage->setHtml($newPage->buildPage());
+
 ?>
 
-
-<script src="js/scripts.js"></script>
-</body>
-</html>
