8727338eb4321ae02800090f84fb142b43c0279f
mohammed abdulai <ma678@njit.edu>
2017-12-04 14:25:29 -0500

    Update:autolad data into form

--- pages/edit_account.php
+++ pages/edit_account.php
@@ -1,4 +1,8 @@
 <?php
+/*session_start();
+if (!isset($_SESSION['username'])) {
+    return header("location: index.php?page=login&action=login");
+}*/
 use utility\htmlTags;
 class edit_account extends utility\page
 {
@@ -16,21 +20,23 @@ $newPage = new edit_account();
 
 $header = '<div class="container"><h1>Edit Acccount</h1></div>';
 
-$editForm = '<div class="container" <form action="index.php?page=createAcct" method="post">
+$userData = get_object_vars($data);
+
+$editForm = '<div class="container" <form action="index.php?page=account&action=save&id=<?php $data[\'id\'] ?>" method="post">
 			<div class="form-group">
 			    <label for="email">First Name:</label>
-			    <input type="text" class="form-control" id="fname">
+			    <input type="text" class="form-control" id="fname" value="'.$userData['fname'].'">
 			</div>
 			<div class="form-group">
 			    <label for="email">Last Name:</label>
-			    <input type="text" class="form-control" id="lname">
+			    <input type="text" class="form-control" id="lname" value="'.$userData['lname'].'">
 			 </div>
 			<div class="form-group">
 			  <label for="email">Email address:</label>
-			  <input type="email" class="form-control" id="email">
+			  <input type="email" class="form-control" id="email" value="' .$userData['email']. '">
 			<div class="form-group">
 			    <label>Phone:</label>
-			    <input type="tel" class="form-control" id="phone">
+			    <input type="tel" class="form-control" id="phone" value="' .$userData['phone']. '">
             </div>
 			</div>
 			<div class="form-group">
