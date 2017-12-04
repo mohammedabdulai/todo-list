77b6dc2d0b75ef7d58c2eabc38b93a1d50a6c7a3
mohammed abdulai <ma678@njit.edu>
2017-12-04 14:26:08 -0500

    Update: autoload data into form

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -1,4 +1,8 @@
 <?php
+/*session_start();
+if (!isset($_SESSION['username'])) {
+    return header("location: index.php?page=login&action=login");
+}*/
 use utility\htmlTags;
 class edit_task extends utility\page
 {
@@ -16,30 +20,37 @@ $newPage = new edit_task();
 
 $header = '<div class="container"><h1>Edit Task</h1></div>';
 
-$editForm = '<div class="container" <form action="index.php?page=createAcct" method="post">
+$taskData = get_object_vars($data);
+
+$editForm = '<div class="container" <form action="index.php?page=task&action=save&id=<?php $data[\'id\'] ?> " method="post">
 			<div class="form-group">
 			  <label for="email">Owner Email:</label>
-			  <input type="email" class="form-control" id="OwnerEmail">
+			  <input type="email" class="form-control" id="OwnerEmail" value="'.$taskData['owneremail'].'">
 			<div class="form-group">
 			    <label for="dueDate">Due Date:</label>
-			    <input type="date" class="form-control" id="dueDate">
+			    <input type="datetime-local" class="form-control" id="dueDate" value="'.$taskData['duedate'].'">
             </div>
 			</div>
 			<div class="form-group">
 			  <label for="message">Message:</label>
-			  <input type="text" class="form-control" id="message">
+			  <input type="text" class="form-control" id="message" value="'.$taskData['message'].'">
 			</div>
 			<div class="form-group">
 			  <label for="isDone">Is Done:</label>
-			  <input type="text" class="form-control" id="isDone">
+			  <input type="text" class="form-control" id="isDone" value="'.$taskData['isdone'].'">
 			</div>
-			<button type="submit" class="btn btn-default">Submit</button>
-			<button type="submit" class="btn btn-default" name="cancel">Cancel</button>
+			<div class="form-group">
+			<button type="submit" class="btn btn-default col-sm-6">Submit</button>
+			<button type="submit" class="btn btn-default col-sm-6" name="cancel">Cancel</button>
+			</div>
+			<br/>
 			</form> 
             <hr>
+            <fieldset class="fieldset"><legend>Action Center</legend>
 			<form action="index.php?page=tasks&action=delete&id=<?php $data[\'id\'] ?> " method="post" id="form1">
                 <button type="submit" class="btn btn-default"form="form1" value="delete">Delete</button>
             </form>
+            </fieldset>
             <hr>
 			</div>';
 
