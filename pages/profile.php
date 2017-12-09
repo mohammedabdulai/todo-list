dda7fda53a00ee624c9d513ae5162684f6658650
mohammed abdulai <ma678@njit.edu>
2017-12-09 01:56:12 -0500

    profile page updaded with links

--- pages/profile.php
+++ pages/profile.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
+/*session_start();
 if (!isset($_SESSION['username'])) {
     return header("location: index.php?page=login&action=login");
-}
+}*/
 use utility\htmlTags;
 class profile extends utility\page
 {
@@ -20,12 +20,25 @@ $newPage = new profile();
 
 $header = '<div class="container"><h1>User Profile</h1></div>';
 
-$links = '<div class="container">
-<h3><a href="index.php?page=tasks&action=all">Show All Tasks</a></h3></div>';
+$actionCenter = '<div class="container">
+            <fieldset class="fieldset"><legend>Action Center</legend>
+            <div class="form-group">
+            <form action="index.php?page=account&action=edit&id=' . $data->id . '" method="get" id="form1">
+                 <button class="btn btn-default col-sm-3" type="submit" form="form1" value="edit"> View/Edit Account </button>
+            </form>
+            <form action="index.php?page=tasks&action=show_all_tasks&id=' . $data->id . ' " method="get" id="form1">
+                <button class="btn btn-default col-sm-3" type="submit" form="form1" value="delete">View Tasks</button>
+            </form>
+            <form action="index.php?page=tasks&action=create&id=' . $data->id . ' " method="get" id="form1">
+                <button class="btn btn-default col-sm-3" type="submit" form="form1" value="newTask"> New Task </button>
+            </form>
+            </div>
+            </fieldset>
+        </div><hr>';
 
 
 $newPage->buildPage($header);
-$newPage->buildPage($links);
+$newPage->buildPage($actionCenter);
 $table= \utility\htmlTable::generateTableFromOneRecord($data);
 $newPage->buildPage($table);
 $newPage->setHtml($newPage->buildPage());
