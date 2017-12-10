775f79f0569d5cce1b7e3ed477cfa428773afd92
mohammed abdulai <ma678@njit.edu>
2017-12-09 19:45:26 -0500

    profile page updated

--- pages/profile.php
+++ pages/profile.php
@@ -26,19 +26,28 @@ $actionCenter = '<div class="container">
             <form action="index.php?page=account&action=edit&id=' . $data->id . '" method="get" id="form1">
                  <button class="btn btn-default col-sm-3" type="submit" form="form1" value="edit"> View/Edit Account </button>
             </form>
-            <form action="index.php?page=tasks&action=show_all_tasks&id=' . $data->id . ' " method="get" id="form1">
-                <button class="btn btn-default col-sm-3" type="submit" form="form1" value="delete">View Tasks</button>
+            <form action="index.php?page=tasks&action=show_all_tasks&id=' . $data->id . ' " method="get" id="form2">
+                <button class="btn btn-default col-sm-3" type="submit" form="form2" value="delete">View Tasks</button>
             </form>
-            <form action="index.php?page=tasks&action=create&id=' . $data->id . ' " method="get" id="form1">
-                <button class="btn btn-default col-sm-3" type="submit" form="form1" value="newTask"> New Task </button>
+            <form action="index.php?page=tasks&action=create&id=' . $data->id . ' " method="get" id="form3">
+                <button class="btn btn-default col-sm-3" type="submit" form="form3" value="newTask"> New Task </button>
             </form>
             </div>
             </fieldset>
         </div><hr>';
 
+$actionCenterDropDown = '<div class="container"><div class="dropdown"><a class="btn btn-primary dropdown-toggle" type="button" id="actionButton" data-toggle="dropdown"><span class="glyphicon glyphicon-th-list"></span> Action Center <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index.php?page=account&action=edit&id=' . $data->id . ' ">Edit</a></li>
+                                <li><a href="index.php?page=tasks&action=show&id=' . $data->id . ' ">View Tasks</a></li>
+                                <li><a href="index.php?page=task&action=create&id=' . $data->id . ' ">New Task</a></li>
+                            </ul>
+                         </div></div>';
+
 
 $newPage->buildPage($header);
-$newPage->buildPage($actionCenter);
+$newPage->buildPage($actionCenterDropDown);
+//$newPage->buildPage($actionCenter);
 $table= \utility\htmlTable::generateTableFromOneRecord($data);
 $newPage->buildPage($table);
 $newPage->setHtml($newPage->buildPage());
