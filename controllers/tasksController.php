17b1b9fcc3cfc95b548910a2e781c84ba040a2f5
mohammed abdulai <ma678@njit.edu>
2017-12-11 20:24:35 -0500

    show_all method updated to check for no task

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kwilliams
- * Date: 11/27/17
- * Time: 5:32 PM
- */
 //each page extends controller and the index.php?page=tasks causes the controller to be called
 class tasksController extends http\controller
 {
@@ -15,7 +9,20 @@ class tasksController extends http\controller
         $record = todos::findOne($_REQUEST['id']);
         self::getTemplate('show_task', $record);
     }
+    //Method to show all logged on user tasks.
+    //If not task yet, user is directed to create a new task.
+    public static function show_all()
+    {
+        $record = todos::findUserTasks($_REQUEST['id']);
+        $arr = (array)$record;
+        if(!empty($arr)){
+            self::getTemplate('show_task', $record);
+        }else{
+            $record = accounts::findOne($_REQUEST['id']);
+            self::getTemplate('new_task', $record);
+        }
 
+    }
     //to call the show function the url is index.php?page=task&action=list_task
     public static function all()
     {
@@ -27,12 +34,14 @@ class tasksController extends http\controller
     //you should check the notes on the project posted in moodle for how to use active record here
     public static function create()
     {
-        self::getTemplate('new_task');
+        $record = accounts::findOne($_REQUEST['id']);
+        self::getTemplate('new_task', $record);
     }
 
     //this is the function to view edit record form
     public static function edit()
     {
+        //$id = todos::findTaskIDbyOwnerID($_REQUEST['id']);
         $record = todos::findOne($_REQUEST['id']);
         self::getTemplate('edit_task', $record);
     }
@@ -43,29 +52,31 @@ class tasksController extends http\controller
         $record = todos::findOne($_REQUEST['id']);
         $record->body = $_REQUEST['body'];
         $record->save();
-        print_r($_POST);
     }
     //this is the delete function.  You actually return the edit form and then there should be 2 forms on that.
     //One form is the todo and the other is just for the delete button
     public static function delete()
     {
         $record = todos::findOne($_REQUEST['id']);
+        $ownerid = $record->ownerid;
         $record->delete();
-        print_r($_POST);
+        $record = todos::findUserTasks($ownerid);
+        self::getTemplate('show_task', $record);
     }
 
     public static function save()
     {
         date_default_timezone_set('America/New_York');
         $record = new todo();
-        $record->owneremail = $_POST['owneremail'];
-        $record->ownerid = $_SESSION['id'];
+        $record->owneremail = $_REQUEST['owneremail'];
+        $record->ownerid = $_REQUEST['id'];
         $record->createddate = date("m/d/Y h:i:sa");
         $record->duedate = $_POST['duedate'];
         $record->message = $_POST['message'];
         $record->isdone = $_POST['isdone'];
         $record->save();
 
+        $record = todos::findUserTasks($_REQUEST['id']);
         self::getTemplate('show_task', $record);
     }
 }
