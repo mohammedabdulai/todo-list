e18e90d995e41e6d4ff9339f625452e95bab73a0
mohammed abdulai <ma678@njit.edu>
2017-12-14 18:03:47 -0500

    pages boiler plate updated

--- core/utility/page.php
+++ core/utility/page.php
@@ -11,12 +11,10 @@ abstract class page
         }
         $this->html .= htmlTags::htmlOpen();
         $this->html .= htmlTags::headOpen();
-        $this->html .= '  <title>todo-list</title>
+        $this->html .= '<title>todo-list</title>
                         <meta charset="utf-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1">
-                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-                        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>';
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">';
         $this->html .= htmlTags::headClose();
         $this->html .= htmlTags::bodyOpen();
         if(!isset($_SESSION['login']))
@@ -37,8 +35,8 @@ abstract class page
                               <li><a href="index.php?page=about">About</a></li>
                             </ul>
                             <ul class="nav navbar-nav navbar-right">
-                              <li><a href="index.php?page=account&action=create"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                              <li><a href="index.php?page=account&action=login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                              <li><a href="index.php?page=accounts&action=create"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                              <li><a href="index.php?page=accounts&action=login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                             </ul>
                           </div>
                         </nav>';
@@ -60,8 +58,8 @@ abstract class page
                               <li><a href="index.php?page=about">About</a></li>
                             </ul>
                             <ul class="nav navbar-nav navbar-right">
-                              <li><a href="index.php?page=account&action=profile"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
-                              <li><a href="index.php?page=account&action=logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                              <li><a href="index.php?page=accounts&action=profile"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+                              <li><a href="index.php?page=accounts&action=logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                             </ul>
                           </div>
                         </nav>';
@@ -79,7 +77,7 @@ abstract class page
     }
     public function __destruct()
     {
-        $this->html .= '<script type="text/javascript">
+        /*$this->html .= '<script type="text/javascript">
         $(document).ready(function(){
             $(".btn").click(function(){
                 //$(this).submit();
@@ -89,7 +87,16 @@ abstract class page
                 }); 
             });   
         });
-        </script>';
+        </script>';*/
+        //Bootstrap Scripts
+         $this->html .='<script>
+                            function goBack() {
+                                window.history.back();
+                            }
+                         </script>';
+        $this->html .= '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+                        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>';
     	$this->html .= htmlTags::bodyClose();
         $this->html .= htmlTags::htmlClose();
         stringFunctions::printThis($this->html);
