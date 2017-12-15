c682be445f94fc2a3bbb312fafa73950751f2c07
mohammed abdulai <ma678@njit.edu>
2017-12-15 11:27:03 -0500

    update link updated

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -1,7 +1,6 @@
 <?php
-/*session_start();
-if (!isset($_SESSION['username'])) {
-    return header("location: index.php?page=login&action=login");
+/*if($_SESSION['login'] == FALSE){
+    header('Location: index.php?page=accounts&action=login');
 }*/
 use utility\htmlTags;
 class edit_task extends utility\page
@@ -22,32 +21,32 @@ $header = '<div class="container"><h1>Edit Task</h1></div>';
 
 $taskData = get_object_vars($data);
 
-$editForm = '<div class="container" <form action="index.php?page=task&action=save&id=<?php $data[\'id\'] ?> " method="post">
+$editForm = '<div class="container"><form action="index.php?page=tasks&action=update&id=' . $taskData['id'] . ' " method="post" id="edit_task">
 			<div class="form-group">
 			  <label for="email">Owner Email:</label>
-			  <input type="email" class="form-control" id="OwnerEmail" value="'.$taskData['owneremail'].'">
-			<div class="form-group">
-			    <label for="dueDate">Due Date:</label>
-			    <input type="datetime-local" class="form-control" id="dueDate" value="'.$taskData['duedate'].'">
-            </div>
+			  <input type="email" class="form-control" id="OwnerEmail" value="'.$taskData['owneremail'].'" name="owneremail" required>
 			</div>
 			<div class="form-group">
+			    <label for="dueDate">Due Date:</label>
+			    <input type="datetime-local" class="form-control" id="dueDate" value="'.$taskData['duedate'].'" name="duedate" required>
+            </div>
+			<div class="form-group">
 			  <label for="message">Message:</label>
-			  <input type="text" class="form-control" id="message" value="'.$taskData['message'].'">
+			  <input type="text" class="form-control" id="message" value="'.$taskData['message'].'" name="message" required>
 			</div>
 			<div class="form-group">
 			  <label for="isDone">Is Done:</label>
-			  <input type="text" class="form-control" id="isDone" value="'.$taskData['isdone'].'">
+			  <input type="number" class="form-control" id="isDone" value="'.$taskData['isdone'].'" name="isdone" required>
 			</div>
 			<div class="form-group">
-			<button type="submit" class="btn btn-default col-sm-6">Save</button>
-			<button type="submit" class="btn btn-default col-sm-6" name="cancel">Cancel</button>
+			<button type="submit" class="btn btn-default col-sm-6" form="edit_task"><span class="glyphicon glyphicon-save"> </span> Save</button>
+			<a href="index.php?page=tasks&action=show_all&id=' . $taskData['ownerid'] . '" class="btn btn-default col-sm-6" name="cancel"><span class="glyphicon glyphicon-remove"> </span>  Cancel</a>
 			</div>
 			<br/>
 			</form> 
             <hr>
             <fieldset class="fieldset"><legend>Action Center</legend>
-			<form action="index.php?page=tasks&action=delete&id=<?php $data[\'id\'] ?> " method="post" id="form1">
+			<form action="index.php?page=tasks&action=delete&id=' . $taskData['id'] . ' " method="post" id="form1">
                 <button type="submit" class="btn btn-default"form="form1" value="delete">Delete</button>
             </form>
             </fieldset>
