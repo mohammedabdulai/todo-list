535c1c11a7f089a23bfd98a2e3baab4813a5ff71
mohammed abdulai <ma678@njit.edu>
2017-12-15 20:25:56 -0500

    edit account page edited

--- pages/edit_account.php
+++ pages/edit_account.php
@@ -1,7 +1,6 @@
 <?php
-/*session_start();
-if (!isset($_SESSION['username'])) {
-    return header("location: index.php?page=login&action=login");
+/*if($_SESSION['login'] == FALSE){
+    header('Location: index.php?page=accounts&action=login');
 }*/
 use utility\htmlTags;
 class edit_account extends utility\page
@@ -22,39 +21,39 @@ $header = '<div class="container"><h1>Edit Acccount</h1></div>';
 
 $userData = get_object_vars($data);
 
-$editForm = '<div class="container" <form action="index.php?page=account&action=save&id=<?php $data[\'id\'] ?>" method="post">
+$editForm = '<div class="container">
+            <form action="index.php?page=accounts&action=save&id='.$userData['id'].'" method="post">
 			<div class="form-group">
 			    <label for="email">First Name:</label>
-			    <input type="text" class="form-control" id="fname" value="'.$userData['fname'].'">
+			    <input type="text" class="form-control" id="fname" value="'.$userData['fname'].'" name="fname" required>
 			</div>
 			<div class="form-group">
 			    <label for="email">Last Name:</label>
-			    <input type="text" class="form-control" id="lname" value="'.$userData['lname'].'">
+			    <input type="text" class="form-control" id="lname" value="'.$userData['lname'].'" name="lname" required>
 			 </div>
 			<div class="form-group">
 			  <label for="email">Email address:</label>
-			  <input type="email" class="form-control" id="email" value="' .$userData['email']. '">
-			<div class="form-group">
-			    <label>Phone:</label>
-			    <input type="tel" class="form-control" id="phone" value="' .$userData['phone']. '">
-            </div>
+			  <input type="email" class="form-control" id="email" value="' .$userData['email']. '" name="email" required>
 			</div>
 			<div class="form-group">
+			    <label>Phone:</label>
+			    <input type="tel" class="form-control" id="phone" value="' .$userData['phone']. '" name="phone" placeholder="Ex: 3334447777">
+            </div>
+			<div class="form-group">
 			  <label for="pwd">Old Password:</label>
-			  <input type="password" class="form-control" id="pwd">
+			  <input type="password" class="form-control" id="pwd" name="password" placeholder="Required to change password">
 			</div>
 			<div class="form-group">
 			  <label for="pwd">New Password:</label>
-			  <input type="password" class="form-control" id="pwd">
+			  <input type="password" class="form-control" id="pwd" name="newPassword" placeholder="Enter ONLY if changing password">
 			</div>
 			<div class="form-group">
 			  <label for="pwd">Confirm Password:</label>
-			  <input type="password" class="form-control" id="ConfirmPwd">
+			  <input type="password" class="form-control" id="ConfirmPwd" name="confirmPassword" placeholder="Must match new password">
 			</div>
-			<button type="submit" class="btn btn-default">Submit</button>
-			<button type="submit" class="btn btn-default" name="cancel">Cancel</button>
-
-			</form> </div>';
+			<button type="submit" class="btn btn-default col-sm-6"><span class="glyphicon glyphicon-save"></span>Save</button>
+			<a class="btn btn-default col-sm-6" href="index.php?page=accounts&action=show" name="cancel"><span class="glyphicon glyphicon-remove"></span>Cancel</a>
+			</form></div>';
 
 $newPage->buildPage($header);
 $newPage->buildPage($editForm);
