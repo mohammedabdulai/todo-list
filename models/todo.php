c2abe4d7ca3041a0d4f309fcd990eb10744df260
mohammed abdulai <ma678@njit.edu>
2017-12-16 21:51:45 -0500

    getClean function added

--- models/todo.php
+++ models/todo.php
@@ -9,10 +9,23 @@ final class todo extends database\model
     public $message;
     public $isdone;
     protected static $modelName = 'todo';
+
     public static function getTablename()
     {
         $tableName = 'todos';
         return $tableName;
+    }
+
+    public function getClean($in)
+    {
+        $out = trim($in);
+        $out = stripslashes($out);
+        $out = htmlspecialchars($out);
+
+        if(empty($out)){
+            $out = '';
+        }
+        return $out;
     }
 }
 ?>
