a0d306fb04b1829ec2be3e7ecb7ddfa16d029d62
mohammed abdulai <ma678@njit.edu>
2017-12-19 12:34:49 -0500

    new task page redesigned

--- pages/new_task.php
+++ pages/new_task.php
@@ -21,28 +21,27 @@ $header = '<div class="container"><h1>Add New Task</h1></div>';
 
 //$taskData = get_object_vars($data);
 
-$editForm = '<div class="container" >
+$editForm = '<div class="container jumbotron" >
+            <div class="row">
+            <div class="col-md-6 col-md-offset-3"
             <form action="index.php?page=tasks&action=save&id= ' . $data->id . ' &owneremail= ' . $data->email . ' " method="post">
 			<div class="form-group">
 			    <label for="dueDate">Due Date:</label>
-			    <input type="datetime-local" class="form-control" id="dueDate" name="duedate" required>
+			    <input type="datetime-local" placeholder="mm/dd/yyy --:-- --" class="form-control" id="dueDate" name="duedate" required>
             </div>
 			<div class="form-group">
 			  <label for="message">Message:</label>
-			  <input type="text" class="form-control" id="message" name="message" required>
+			  <input type="text"placeholder="Enter task here..." class="form-control" id="message" name="message" required>
 			</div>
 			<div class="form-group">
-			  <label for="isDone">Is Done:</label>
-			  <input type="text" class="form-control" id="isdone" name="isdone" required>
-			</div>
-			<div class="form-group">
-			<button type="submit" class="btn btn-default col-sm-6">Save</button>
-			<a href="index.php?page=tasks&action=show_all&id=' . $data->id . '" class="btn btn-default col-sm-6" name="cancel">Cancel</a>
+			<button type="submit" class="btn btn-default col-sm-6"><span class="glyphicon glyphicon-save"></span> Save</button>
+			<a href="index.php?page=tasks&action=show_all&id=' . $data->id . '" class="btn btn-default col-sm-6" name="cancel"><span class="glyphicon glyphicon-remove"></span> Cancel</a>
 			</div>
 			<br/>
-			</form> 
-            <hr>
-			</div>';
+			</form>
+			</div>
+			</div> 
+			</div><hr>';
 
 
 $newPage->buildPage($header);
