2dc302c497f880cc1bf4b3d86ae2cb9d90cf4d14
mohammed abdulai <ma678@njit.edu>
2017-12-20 08:33:19 -0500

    save method updated with strong validation and comments added

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -92,31 +92,116 @@ class accountsController extends http\controller
     }
     public static function save()
     {
+        $message ='';
+        $error = FALSE;
+        //grab the users account object and save as record
         $id = $_SESSION['id'];
         $record = accounts::findOne($id);
+        //Clean up user input with getClean function and set to user object property accordingly
+        //Required fields
+        //Validate and set email
+        if(!filter_var($record->getClean($_REQUEST['email']), FILTER_VALIDATE_EMAIL)){
+            $message .= '<div class="container text-danger"><b>Invalid email entered!</b></div><br>';
+            $error = TRUE;
+        }else{
+            $record->email = $record->getClean($_REQUEST['email']);
+        }
+        //Validate and set first name
+        if($record->getClean($_REQUEST['fname']) == '' || strlen($record->getClean($_REQUEST['fname']))<2){
+            $message .= '<div class="container text-danger"><b>Invalid first name! Name must be at least two characters</b></div><br>';
+            $error = TRUE;
+        }elseif(is_numeric($_REQUEST['fname'])){
+            $message .= '<div class="container text-danger"><b>Numeric entered! Name must be at least two characters</b></div><br>';
+            $error = TRUE;
+        }else{
+            $record->fname = $record->getClean($_REQUEST['fname']);
+        }
+        //Validate and set last name
+        if($record->getClean($_REQUEST['lname']) == '' || strlen($record->getClean($_REQUEST['lname']))<2){
+            $message .= '<div class="container text-danger"><b>Invalid last name! First name must be at least two characters</b></div><br>';
+            $error = TRUE;
+        } elseif(is_numeric($_REQUEST['lname'])){
+            $message .= '<div class="container text-danger"><b>Numeric entered! Last name must be at least two characters</b></div><br>';
+            $error = TRUE;
+        }else {
+            $record->lname = $record->getClean($_REQUEST['lname']);
+        }
+        //Check if optional fields are set and get clean version of inputs.
+        //If optional fields are not set, the the object property to empty string for database acceptance.
+        //Check and set phone
+        if(isset($_REQUEST['phone'])){
+            $record->phone = $record->getClean($_REQUEST['phone']);
+        }else{$record->phone = '';}
+        //Checks and set gender
+        if(isset($_REQUEST['gender'])){
+            $record->gender = $record->getClean($_REQUEST['gender']);
+        }else{$record->gender = '';}
+        //Checks and set birthday
+        if(isset($_REQUEST['birthday'])){
+            $record->birthday = $record->getClean($_REQUEST['birthday']);
+        }else{$record->birthday = '';}
 
-        $record->email = $record->getClean($_POST['email']);
-        $record->fname = $record->getClean($_POST['fname']);
-        $record->lname = $record->getClean($_POST['lname']);
-        $record->phone = $record->getClean($_POST['phone']);
-        $record->gender = '';
-        $record->birthday = '';
-        $record->password = $_SESSION['user']->password;
-
-        if(isset($_POST['password'])){
-            $correct = password_verify($record->getClean($_POST['password']), $record->password);
+        //Check if old password is entered by user then compared with hashed password stored in the database.
+        //If there's a match, then compare the new password with the confirm password field.
+        if(isset($_REQUEST['password']) && !empty($_REQUEST['password'])){
+            $correct = password_verify($record->getClean($_REQUEST['password']), $record->password);
+            //if entered password matches with hashed password in the database, compare new password fields
+            //If new password and confirm password fields match, hash the new password and set to password property of object
             if($correct){
-                if($record->getClean($_POST['newPassword'])=== $record->getClean($_POST['confirmPassword'])){
-                    $record->password = password_hash($record->getClean($_POST["newPassword"]),PASSWORD_BCRYPT);
+                if($record->getClean($_REQUEST['newPassword'])=== $record->getClean($_REQUEST['confirmPassword'])&& !empty($_REQUEST['newPassword'])){
+                    $record->password = password_hash($record->getClean($_REQUEST["newPassword"]),PASSWORD_BCRYPT);
+                //If new and confirm password fields don't match, prompt user with warming error message
+                }else{
+                    $message .= '<div class="container text-danger"><b>New password and confirm password don\'t match</b></div><br>';
+                    $error = TRUE;
+                }
+            //Throw error message if old password entered by user doesn't match hashed password stored in database
+            }else{
+                $message .= '<div class="container text-danger"><b>Warning! You must entered old password to change your password</b></div><br>';
+                $error = TRUE;
+            }
+        //If password field is not set, reset password property with hashed password stored for database acceptance of update.
+        }else{$record->password = $_SESSION['user']->password;}
+        //Check if new password fields contain value and old password is not set.
+        if(isset($_REQUEST['newPassword'])&& !empty($_REQUEST['newPassword'])){
+            if(isset($_REQUEST['confirmPassword']) && !empty($_REQUEST['confirmPassword'])){
+                if(!isset($_REQUEST['password']) || empty($_REQUEST['password'])){
+                    $message .='<div class="container text-danger"><b>Old password must be entered to change password</b></div><br>';
+                    $error = TRUE;
                 }
             }
-
+        }
+        //Check if confirm password field is set but new password field is not
+        if(isset($_REQUEST['confirmPassword'])&& !empty($_REQUEST['confirmPassword'])){
+            if(!isset($_REQUEST['newPassword']) || empty($_REQUEST['newPassword'])){
+                $message .='<div class="container text-danger"><b>New password must match confirm password</b></div><br>';
+                $error = TRUE;
+            }
+        }
+        //Check if new password filed is set but confirm password field is not.
+        if(isset($_REQUEST['newPassword'])&& !empty($_REQUEST['newPassword'])){
+            if(!isset($_REQUEST['confirmPassword']) || empty($_REQUEST['confirmPassword'])){
+                $message .='<div class="container text-danger"><b>New password must match confirm password</b></div><br>';
+                $error = TRUE;
+            }
         }
 
-        //print_r($record);
-        $record->save();
-        $record = accounts::findOne($_SESSION['id']);
-        self::getTemplate('profile', $record);
+        //Check if there were any errors in validation.
+        //If not, account data gets saved.
+        if(!$error){
+            //Save updated user account
+            $record->save();
+            //Grab the updated account and display to user
+            $record = accounts::findOne($_SESSION['id']);
+            self::getTemplate('profile', $record);
+        //If there was any validation error, send user back to edit form with account data and error messages.
+        }else{
+            $arr[0]= accounts::findOne($_SESSION['id']);
+            $arr[1] = $message;
+            self::getTemplate('edit_account', $arr);
+        }
+
+
     }
     //this is to login, here is where you find the account and allow login or deny.
     public static function login()
@@ -128,12 +213,18 @@ class accountsController extends http\controller
     {
         self::getTemplate('signUp');
     }
-
+    //Loads a signed on users profile.
+    //If not signed on, visitor is directed to login.
     public static function profile()
     {
-        $id = $_SESSION['id'];
-        $record = accounts::findOne($id);
-        self::getTemplate('profile', $record);
+        if(!isset($_SESSION['id'])){
+            $message = '<div class="container text-danger"><b>You must login to view profile</b></div>';
+            self::getTemplate('login', $message);
+        }else{
+            $id = $_SESSION['id'];
+            $record = accounts::findOne($id);
+            self::getTemplate('profile', $record);
+        }
     }
     public static function logout()
     {
